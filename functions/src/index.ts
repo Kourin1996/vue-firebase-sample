beeca11a7909f87ebc3f6ce4a8627d7475ed2fa0
kourin <kourin1996@gmail.com>
2020-10-22 22:26:12 +0800

    test: add new function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,9 @@
 import * as functions from "firebase-functions";
 
 export const hello = functions.https.onRequest((request, response) => {
+  response.status(200).send("Hello");
+});
+
+export const helloWorld = functions.https.onRequest((request, response) => {
   response.status(200).send("Hello World");
 });
